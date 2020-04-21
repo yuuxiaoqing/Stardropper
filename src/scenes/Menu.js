c431aba9dee53266a5ffbdd31c59356169bede7e
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-04-21 08:20:16 -0700

    playing around with extra stuff, not for the assignment

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -33,7 +33,8 @@ class Menu extends Phaser.Scene{
             this.add.text(50, 30, '2 Player Mode').setOrigin(0,0);
             player2 = true;
         }
-    
+        
+        //tried to make it so it can toggles between two scenes..but doesn't work
 
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)){
             //EASY level
